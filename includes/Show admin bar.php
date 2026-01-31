1279d20888b86323164f3d870a6b75456b715fbe
Phrilly <pirip.dunne@gmail.com>
2026-01-31 18:21:39 +0000

    Update Show admin bar.php

--- includes/Show admin bar.php
+++ includes/Show admin bar.php
@@ -1,3 +1,4 @@
-add_filter('show_admin_bar', function ($show) {
+<?php
+add_filter('show_admin_bar', function($show) {
     return current_user_can('manage_options') ? $show : false;
-}, 10, 1);
+});
