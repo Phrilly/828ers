d4464271829b41125811545873df310170b25db8
Phrilly <pirip.dunne@gmail.com>
2026-04-05 23:54:00 +0100

    Revert "remove tee id restrictions"
    This reverts commit 1c15c1999d8bfa7467d33844826a9b02d62854ff.

--- sql/view_handicap_index.sql
+++ sql/view_handicap_index.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS `view_handicap_index`;
 -- END_QUERY
 
--- WATERMARK 1.0.68
+-- WATERMARK 1.0.67
 CREATE OR REPLACE ALGORITHM = UNDEFINED VIEW `view_handicap_index` AS
 select
     `p`.`player_id` AS `player_id`,
@@ -27,9 +27,12 @@ left join (
         select
             `prev`.`hcp_after`
         from
-            `wp_golf_handicap_history` `prev`
+            (`wp_golf_handicap_history` `prev`
+        join `wp_golf_scores` `ps` on
+            (`prev`.`score_id` = `ps`.`score_id`))
         where
             `prev`.`player_id` = `last`.`player_id`
+            and `ps`.`tee_id` in (1, 2, 3)
             and (
                 `prev`.`date_played` < `last`.`date_played`
                 or (
@@ -45,9 +48,12 @@ left join (
         select
             least(count(0), 20)
         from
-            `wp_golf_handicap_history` `h2`
+            (`wp_golf_handicap_history` `h2`
+        join `wp_golf_scores` `s2` on
+            (`h2`.`score_id` = `s2`.`score_id`))
         where
             `h2`.`player_id` = `last`.`player_id`
+            and `s2`.`tee_id` in (1, 2, 3)
             and (
                 `h2`.`date_played` < `last`.`date_played`
                 or (
@@ -57,13 +63,19 @@ left join (
             )
         ) AS `rounds_in_window`
     from
-        ((`wp_golf_handicap_history` `last`
+        (((`wp_golf_handicap_history` `last`
+    join `wp_golf_scores` `s` on
+        (`last`.`score_id` = `s`.`score_id`))
     join (
         select
             `hh`.`player_id` AS `player_id`,
             max(`hh`.`date_played`) AS `max_date`
         from
-            `wp_golf_handicap_history` `hh`
+            (`wp_golf_handicap_history` `hh`
+        join `wp_golf_scores` `ss` on
+            (`hh`.`score_id` = `ss`.`score_id`))
+        where
+            `ss`.`tee_id` in (1, 2, 3)
         group by
             `hh`.`player_id`) `mx` on
         (`mx`.`player_id` = `last`.`player_id` and `mx`.`max_date` = `last`.`date_played`))
@@ -73,12 +85,18 @@ left join (
             `hh`.`date_played` AS `date_played`,
             max(`hh`.`score_id`) AS `max_score_id`
         from
-            `wp_golf_handicap_history` `hh`
+            (`wp_golf_handicap_history` `hh`
+        join `wp_golf_scores` `ss` on
+            (`hh`.`score_id` = `ss`.`score_id`))
+        where
+            `ss`.`tee_id` in (1, 2, 3)
         group by
             `hh`.`player_id`,
             `hh`.`date_played`) `ms` on
         (`ms`.`player_id` = `last`.`player_id`
          and `ms`.`date_played` = `last`.`date_played`
-         and `ms`.`max_score_id` = `last`.`score_id`)) `h` on
+         and `ms`.`max_score_id` = `last`.`score_id`))
+    where
+        `s`.`tee_id` in (1, 2, 3)) `h` on
     (`h`.`player_id` = `p`.`player_id`));
 -- END_QUERY
