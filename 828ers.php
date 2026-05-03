e3dc4636d2b69ee98f9e7a6e32a43b39e3bbf6f0
Phrilly <pirip.dunne@gmail.com>
2026-05-03 11:57:50 +0100

    .sql

--- 828ers.php
+++ 828ers.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: 828ers Golf Handicap System
  * Description: Automated WHS Handicap Tracking, Dashboards, and Git-Triggered Migrations.
- * Version:     1.1.11
+ * Version:     1.1.12
  * Author:      Philip Dunne
  */
 
