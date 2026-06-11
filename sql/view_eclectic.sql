43bd7730337698dd439a4e18446d070fdb0c5da4
Phrilly <pirip.dunne@gmail.com>
2026-06-11 10:21:20 +0100

    Refactor view_eclectic SQL to replace view and clean up comments

--- sql/view_eclectic.sql
+++ sql/view_eclectic.sql
@@ -1,6 +1,7 @@
 DROP VIEW IF EXISTS `view_eclectic`;
+-- END_QUERY
 
-CREATE VIEW `view_eclectic` AS
+CREATE OR REPLACE VIEW `view_eclectic` AS
 SELECT
     p.player_id,
     p.name AS player_name,
@@ -10,7 +11,6 @@ SELECT
     YEAR(s.date_played) AS year_num,
     MIN(hs.gross_score) AS best_gross,
 
-    -- 1. Default 5/8th (0.625) Calculation
     MAX(
         GREATEST(
             0,
@@ -26,7 +26,6 @@ SELECT
         )
     ) AS best_stableford,
 
-    -- 2. New 1/2 (0.50) Calculation
     MAX(
         GREATEST(
             0,
@@ -42,7 +41,6 @@ SELECT
         )
     ) AS best_stableford_50,
 
-    -- 3. New 3/4 (0.75) Calculation
     MAX(
         GREATEST(
             0,
@@ -88,3 +86,4 @@ GROUP BY
     h.par,
     MONTH(s.date_played),
     YEAR(s.date_played);
+-- END_QUERY
